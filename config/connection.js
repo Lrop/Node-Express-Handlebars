941eb5546cf6f5bfa0b6adb3d56de59247221aae
Lrop <Lagropner04@gmail.com>
2019-08-22 11:52:58 -0600

    save

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,43 @@
 // Starting with declaring the dependencies
 const mysql = require("mysql");
 
-
-// Creating Connection To Mysql
-  var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
     database: "burgers_db"
   });
-
-// Console logging the status of the connection 
-  connection.connect(function(err){
-    if (err) {
-      console.error("error connection: " + err.stack);
-      return;
-    }
-    console.log("connected as id: " + connection.threadId);
-  })
-
-// Calling the connection  
-// connection.connect();
-
-// Exporting the Connection
+}
+connection.connect();
 module.exports = connection;
+
+
+
+
+
+
+// // Creating Connection To Mysql
+//   var connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+//   });
+
+// // Console logging the status of the connection 
+//   connection.connect(function(err){
+//     if (err) {
+//       console.error("error connection: " + err.stack);
+//       return;
+//     }
+//     console.log("connected as id: " + connection.threadId);
+//   })
+
+// // Calling the connection  
+// // connection.connect();
+
+// // Exporting the Connection
+// module.exports = connection;
