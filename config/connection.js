3290169a79676ce8d16b04d33e64ea7857c9cd1e
Lrop <Lagropner04@gmail.com>
2019-08-23 13:21:42 -0600

    final commit

--- config/connection.js
+++ config/connection.js
@@ -15,29 +15,3 @@ connection.connect();
 module.exports = connection;
 
 
-
-
-
-
-// // Creating Connection To Mysql
-//   var connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "password",
-//     database: "burgers_db"
-//   });
-
-// // Console logging the status of the connection 
-//   connection.connect(function(err){
-//     if (err) {
-//       console.error("error connection: " + err.stack);
-//       return;
-//     }
-//     console.log("connected as id: " + connection.threadId);
-//   })
-
-// // Calling the connection  
-// // connection.connect();
-
-// // Exporting the Connection
-// module.exports = connection;
